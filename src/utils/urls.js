2c77f67a774d14db0b88f02f50d84f69e0c3ab72
Dipankar-123 <dipankaryadav1234@gmail.com>
2025-01-22 12:48:56 +0530

    changes

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -2,7 +2,7 @@
 
 const BASE_URLS = {
     development: "http://localhost:4000",
-    production: "https://billing-prototype-nest.vercel.app/",  
+    production: "https://billing-prototype-nest.vercel.app",  
   };
   
   export default BASE_URLS;
