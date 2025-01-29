9c7b84ec5a515d36d4579d81c297a1e53b244fd0
Nitin Pathak <nitinpathak337@gmail.com>
2025-01-29 22:45:00 +0530

    quickbooks integration fe

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import QuickBooksCallback from './Components/QuickBooksCallback';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/quickbooks/callback" element={<QuickBooksCallback />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
